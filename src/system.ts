4f2beed370ba9504dabd8661747c34bfed3f090a
Matti Ahinko <matti@ahinko.net>
2019-11-26 20:33:40 +0200

    Add first version of JS function call

--- src/system.ts
+++ src/system.ts
@@ -3,8 +3,40 @@ import { SystemFunctionNode } from "./ast/function_node"
 import { ASTNode } from "./ast/ast_node"
 
 export function addSystemFunctions(env: Environment) {
-  env.symbols.prn = new SystemFunctionNode("prn", (args: ASTNode[]) => {
-    console.log(args.map(a => a.toString()).join(" "))
-    return null
-  })
+  env.symbols.import = new SystemFunctionNode(
+    "import",
+    (env: Environment, args: ASTNode[]) => {
+      const ns = args[0].toString().split("/")
+      const alias = args[1].toString()
+      if (ns.length > 2) {
+        throw new Error(
+          "No deeper than one level require is currently supported"
+        )
+      }
+      if (ns[0] === "js") {
+        const anyGlobal = global as any
+        const importNs = anyGlobal[ns[1]]
+        const fnKeys = Object.keys(importNs).filter(
+          k => typeof importNs[k] === "function"
+        )
+        fnKeys.forEach(
+          k =>
+            (env.symbols[`${alias}/${k}`] = new SystemFunctionNode(
+              k,
+              (env: Environment, args: ASTNode[]) => {
+                importNs[k].apply(
+                  null,
+                  args.map(a => a.getTypedValue())
+                )
+
+                return null
+              }
+            ))
+        )
+      } else {
+        throw new Error("Non-JS require not implemented yet")
+      }
+      return null
+    }
+  )
 }
