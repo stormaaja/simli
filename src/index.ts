cb5ef1fcd0b0acb12f3076a21d12f24e1103c0fb
Matti Ahinko <matti@ahinko.net>
2019-11-27 18:19:38 +0200

    Move parsing file

--- src/index.ts
+++ src/index.ts
@@ -3,13 +3,17 @@ import { Reader, fileExists } from "./parser/reader"
 import { Environment } from "./ast/environment"
 import { addSystemFunctions } from "./system"
 
+export function parseFile(filePath: string) {
+  return parse(new Reader(filePath))
+}
+
 function main(args: string[]) {
   if (args.length < 3) {
     console.error("Not enough params")
   } else {
     const filePath = args[2]
     if (fileExists(filePath)) {
-      const ast = parse(new Reader(filePath))
+      const ast = parseFile(filePath)
       if (args.indexOf("--debug") > -1) {
         console.log(JSON.stringify(ast, null, 2))
       }
